f5dd2214ed754ea0fc796adc58b29b91f8cd1646
seokhwan-an <asok369258@naver.com>
2024-08-05 22:33:37 +0900

    project init

---
+++ src/main/java/choorai/retrospect/RetrospectApplication.java
@@ -0,0 +1,13 @@
+package choorai.retrospect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RetrospectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RetrospectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/choorai/retrospect/RetrospectApplicationTests.java
@@ -0,0 +1,13 @@
+package choorai.retrospect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RetrospectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
